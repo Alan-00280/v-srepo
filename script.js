4115177725e0f41fda5029d583b26305eff679cb
alan <alanperdana08@gmail.com>
2025-02-14 13:27:34 +0700

    update-1

--- script.js
+++ script.js
@@ -1,13 +1,65 @@
 const text = `Selamat hari valentine sayangku cintaku manisku. Semoga tetep sayang yaaa. Muachhh.... 💐💕`;
 
+const lyrics = `
+Kita usahakan rumah itu<br>
+Dari depan akan tampak sederhana<br>
+Tapi kebunnya luas<br>
+Tanamannya mewah, megah<br>
+<br>
+Kita usahakan rumah itu<br>
+Dari depan akan tampak sederhana<br>
+Tapi dibuat kuat<br>
+Dirancang muat, lega<br>
+<br>
+Urusan perabotan dan wangi-wangian<br>
+Kuserahkan pada s'leramu yang lebih maju<br>
+Tapi tata ruang, aku ikut pertimbangkan<br>
+Kar'na kalau nanti kita punya kesibukan<br>
+<br>
+Malam tetap kumpul di meja panjang<br>
+Ruang makan kita<br>
+Berbincang tentang hari yang panjang<br>
+<br>
+Kita usahakan rumah itu<br>
+Dari depan akan tampak sederhana<br>
+Tapi penerangannya<br>
+Diracik begitu romantis<br>
+<br>
+Urusan perabotan dan wangi-wangian<br>
+Kuserahkan pada s'leramu yang lebih maju<br>
+Tapi tata ruang, aku ikut pertimbangkan<br>
+Kar'na kalau nanti kita punya kesibukan<br>
+<br>
+Malam tetap kumpul di meja panjang<br>
+Ruang makan kita<br>
+Berbincang tentang hari yang panjang<br>
+<br>
+Boleh kamu keliling dunia<br>
+Dan temukan banyak tempat-tempat 'tuk singgah<br>
+Sementara<br>
+<br>
+Kamu boleh namai itu rumah<br>
+Selama ada m'reka yang kamu cinta<br>
+Di dalamnya<br>
+<br>
+Kita usahakan rumah itu<br>
+`;
+
 function fun1() {
-    
+    document.querySelector('#main-img').classList.replace('normal', 'small');
+    document.querySelector('.reg-text').classList.add('hide');
+    document.querySelector('.card').innerHTML =
+    `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/1x9jnpPOAMSrr7DuIG5jMl?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy" class="song"></iframe>
+    <p class="lyric-text">${lyrics}</p>
+    <button class="button-lyric" onclick="fun2()">hide</button>
+    <a class="button-2" href="./index.html">Back</a>`;
 }
 
 document.querySelector('.button').addEventListener(
     'click', ()=>{
         document.querySelector('.card').innerHTML =
         `<p class="reg-text">${text}</p>
-        <a class="button-2" href="./index.html">Click Me!</a>`;
+        
+        <button class="button" onclick="fun1()">Next</button>`;
     }
 );
